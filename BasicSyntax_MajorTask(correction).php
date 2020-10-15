6532dba31cd58ee96acf8cc52669896d308cd3bb
Evans Obi <32423919+VnsObi@users.noreply.github.com>
2020-10-15 14:15:08 +0100

    This is the updated Major task file

---
+++ BasicSyntax_MajorTask(correction).php
@@ -0,0 +1,25 @@
+<?php
+//===== First Major Task =====
+
+/*
+Occupation: Tech Engineer
+Gender: Male
+Sidehustle username: vnsobi
+*/
+
+//=== About Me Tag ===
+echo "<h1>About Me</h1> <hr/>";
+
+//=== My sidehustle Internship username ===
+echo "<h3>vnsobi</h3>";
+
+//=== A little summary about myself ===
+echo "<p>
+        Am Evans Obi, graduated from AAU last year in Computer Science.
+        Am currently self employed working as a tech Engineer i.e phone and 
+        computer repairs. I majored in Computer Networking and Cyber Security.
+        Am more of a techy guy, but i know that i still have much to learn in tech world.
+        These are just the brief summary about me.
+    </p>";
+
+?>
